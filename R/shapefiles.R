98fbd184d0733ca12285388e3aa56a03e33fbaec
hubcad25 <hubertcadieux@gmail.com>
2025-02-17 15:50:03 -0500

    add canada 2022 spatial data

---
+++ R/shapefiles.R
@@ -0,0 +1,7 @@
+#' Données géospatiales du Canada – Circonscriptions électorales (redécoupage de 2022)
+#'
+#' Objet sf contenant les circonscriptions électorales du Canada, fusionnées à partir de plusieurs sources.
+#'
+#' @format Un objet `sf` avec une colonne `geometry` représentant les polygones des circonscriptions.
+#' @source Statistique Canada
+"spatial_canada_2022_electoral_ridings"
--- data-raw/canada_2022/02_load_shapefiles_as_rda_to_package.R
+++ data-raw/canada_2022/02_load_shapefiles_as_rda_to_package.R
@@ -1,6 +1,9 @@
 ## This script loads the shapefiles from each province and merges them together.
 ## To obtain access to the data folder containing the downloaded shapefiles, contact Hubert Cadieux
 
+# Packages ---------------------------------------------------------------
+library(dplyr)
+
 # List files to merge together -------------------------------------------------------------
 files <- list.files(
   "data-raw/data/canada_2022/shapefiles/",
@@ -19,11 +22,49 @@ df_shapefiles_together <- lapply(
     return(df)
   }
 ) |> 
-  dplyr::bind_rows()
+  dplyr::bind_rows() |> 
+  select(
+    id_riding = FED_NUM,
+    name_riding_en = ED_NAMEE,
+    name_riding_fr = ED_NAMEF,
+    id_province = PROV_CODE,
+    geometry
+  )
 
 #### Il semble avoir des doublons, mais après vérifications
 #### c'est simplement pour gérer les circonscriptions où le territoire est séparé par de l'eau ou wtv.
 #### Exemple: 24020 et 24023 (Côte-Nord et Gaspésie)
 
-
 ### AJOUTER LES TERRITOIRES
+#### J'ai manuellement downloadé et unzippé les fichiers dans "data-raw/data/canada_2013".
+#### Ce dossier contient les shapefiles des circonscriptions du redécoupage de 2013,
+#### et il contient donc les circonscriptions des territoires (qui n'ont pas changé en 2022).
+
+df_territories_shapefiles <- sf::st_read("data-raw/data/canada_2013/decoupage_2013.shp") |> 
+  ## filter for territories only
+  filter(
+    `CÉFIDU` %in% c(
+      60001,
+      61001,
+      62001
+      )
+  ) |> 
+  mutate(
+    id_province = case_when(
+      `CÉFIDU` == 60001 ~ "YT",
+      `CÉFIDU` == 61001 ~ "NT",
+      `CÉFIDU` == 62001 ~ "NU"
+    )
+  ) |> 
+  select(
+    id_riding = `CÉFIDU`,
+    name_riding_en = `CÉFANOM`,
+    name_riding_fr = `CÉFFNOM`,
+    id_province,
+    geometry
+  )
+  
+
+df_output <- rbind(df_shapefiles_together, df_territories_shapefiles)
+
+save(df_output, file = "data/spatial_canada_2022_electoral_ridings.rda", compress = "bzip2")
