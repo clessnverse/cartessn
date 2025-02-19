53f1c719a74f8d095b09bef25d39ee51d1cbace5
hubcad25 <hubertcadieux@gmail.com>
2025-02-19 12:22:31 -0500

    add 2013 names

---
+++ R/names_canada_2013_electoral_ridings.R
@@ -0,0 +1,13 @@
+#' Names of Canadian Electoral Ridings (2013 Redistribution)
+#'
+#' This dataset contains the names and identifiers of Canadian electoral ridings based on the 2013 redistribution.
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   \item{id_riding}{Character. The unique identifier for each riding.}
+#'   \item{name_riding_en}{Character. The name of the riding in English.}
+#'   \item{name_riding_fr}{Character. The name of the riding in French.}
+#'   \item{id_province}{Character. The identifier of the province to which the riding belongs.}
+#' }
+#' @source Statistics Canada
+"names_canada_2013_electoral_ridings"
---
+++ R/names_canada_2022_electoral_ridings.R
@@ -0,0 +1,13 @@
+#' Names of Canadian Electoral Ridings (2022 Redistribution)
+#'
+#' This dataset contains the names and identifiers of Canadian electoral ridings based on the 2022 redistribution.
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'   \item{id_riding}{Character. The unique identifier for each riding.}
+#'   \item{name_riding_en}{Character. The name of the riding in English.}
+#'   \item{name_riding_fr}{Character. The name of the riding in French.}
+#'   \item{id_province}{Character. The identifier of the province to which the riding belongs.}
+#' }
+#' @source Statistics Canada
+"names_canada_2022_electoral_ridings"
