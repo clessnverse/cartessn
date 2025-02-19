f1e892d33c2df3f07c9d173f521c67289925ed40
hubcad25 <hubertcadieux@gmail.com>
2025-02-18 22:44:16 -0500

    include spatial canada rta in package

---
+++ R/spatial_canada_2021_rta.R
@@ -0,0 +1,7 @@
+#' Données géospatiales du Canada – régions de tri d'acheminement (RTA)
+#'
+#' Objet sf contenant les régions de tri d'acheminement du Canada (RTA).
+#'
+#' @format Un objet `sf` avec une colonne `geometry` représentant les polygones des RTA
+#' @source Statistique Canada
+"spatial_canada_2021_rta"
