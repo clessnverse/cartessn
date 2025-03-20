874f09323a3e7fcc3a94a1aef72441ca2c605a7b
hubcad25 <hubertcadieux@gmail.com>
2025-03-19 23:59:53 -0400

    extract census by riding

---
+++ data-raw/census_canada_2022_electoral_ridings/01_extract_from_statcan.R
@@ -0,0 +1,14 @@
+### Ce script sert à aller extraire les données de
+#### recensement par circonscription sur Stat Can
+
+url <- "https://www12.statcan.gc.ca/census-recensement/2021/dp-pd/prof/details/download-telecharger/comp/GetFile.cfm?Lang=F&FILETYPE=CSV&GEONO=029"
+destination_folder <- "data-raw/data/census_canada_2022_electoral_ridings"
+destination_file <- paste0(destination_folder, "/census_data.zip")
+
+if (!dir.exists(destination_folder)) {
+  dir.create(destination_folder, recursive = TRUE)
+}
+
+download.file(url, destfile = destination_file)
+unzip(destination_file, exdir = destination_folder)
+unlink(destination_file)
