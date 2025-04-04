33dbe1c034b05b0dd938634cd2429bd34889ecec
etienneprx <etienne.proulx.2@ulaval.ca>
2025-04-04 19:14:30 -0400

    fix spatial 2022 and names

--- data-raw/canada_2022_electoral_ridings/01_extract_shapefiles.R
+++ data-raw/canada_2022_electoral_ridings/01_extract_shapefiles.R
@@ -1,65 +1,36 @@
-## This script extracts the zip files containing the shapefiles from each province and saves them in the data folder
-## The shapefiles from each province can be accessed here: https://redecoupage-redistribution-2022.ca/com/index_e.aspx
+## This script downloads and extracts the shapefiles for the 2023 Canadian federal electoral districts
+## The shapefiles can be accessed here: https://ftp.cartes.canada.ca/pub/elections_elections/Electoral-districts_Circonscription-electorale/Elections_Canada_2021/FED_CA_2021_FR.gdb.zip
 
 # Packages ---------------------------------------------------------------
-library(rvest)
+library(utils)
 
-# Scrape provinces ids in the website ------------------------------------
+# Download and extract shapefile -----------------------------------------
 
-### load page
-page <- read_html("https://redecoupage-redistribution-2022.ca/com/index_e.aspx")
+# URL for the FED_CA_2021_FR.gdb.zip file
+zip_url <- "https://ftp.maps.canada.ca/pub/elections_elections/Electoral-districts_Circonscription-electorale/federal_electoral_districts_boundaries_2023/CF_CA_2023_FR-SHP.zip"
 
-### extract all urls
-all_urls <- page |> 
-  html_nodes("a") |> 
-  html_attr("href") |> 
-  unique()
+# Define destination paths
+destination_folder <- "data-raw/data/canada_2022_electoral_ridings/"
+zip_destination <- file.path(destination_folder, "CF_CA_2023_FR-SHP.zip")
+shp_folder_path <- "data-raw/CF_CA_2023_FR-SHP"
 
-### filter to only keep urls that are links to provinces pages
-provinces_urls <- all_urls[grepl("^/com/[a-z]{2,3}/index_e\\.aspx$", all_urls)]
-
-### extract the provinces ids from these urls
-provinces <- sub("^/com/([a-z]{2,3})/.*", "\\1", provinces_urls)
-
-# Extract zip files containing shapefiles ------------------------------------------------------
-
-## example url: https://redecoupage-redistribution-2022.ca/com/nl/NLpropo.zip
-
-destination_folder <- "data-raw/data/canada_2022_electoral_ridings/shapefiles/"
-
-## Only try to extract provinces that are not yet extracted
-provinces_extracted <- gsub("\\.zip", "", list.files(destination_folder))
-provinces_not_extacted <- provinces[!(provinces %in% provinces_extracted)]
-
-for (i in provinces_not_extacted){
-  zip_urls <- paste0(
-    "https://redecoupage-redistribution-2022.ca/com/",
-    i,
-    "/",
-    toupper(substr(i, 1, 2)),
-    c("final.zip", ".zip")
-  )
-  ### If the destination folder does not exist, create.
-  if (!dir.exists(destination_folder)) {
-    dir.create(destination_folder, recursive = TRUE)
-  }
-  ### download zip file
-  tryCatch(
-    expr = download.file(zip_urls[1], destfile = paste0(destination_folder, i, ".zip")),
-    error = function(e) download.file(zip_urls[2], destfile = paste0(destination_folder, i, ".zip"))
-  )
-  message("-------------------\n", i, "\n-------------------")
-  Sys.sleep(1)
+# Create destination directory if it doesn't exist
+if (!dir.exists(destination_folder)) {
+  dir.create(destination_folder, recursive = TRUE)
 }
 
-# Extract zip files as normal folders ------------------------------------
+if (!dir.exists(shp_folder_path)) {
+  dir.create(shp_folder_path, recursive = TRUE)
+}
 
-zip_files <- list.files(destination_folder, pattern = "\\.zip$", full.names = TRUE)
+# Download the zip file
+download.file(zip_url, destfile = zip_destination)
 
-# Extraire chaque ZIP dans un dossier du même nom (sans .zip)
-lapply(zip_files, function(zip) {
-  dossier_sortie <- file.path(destination_folder, tools::file_path_sans_ext(basename(zip)))
-  dir.create(dossier_sortie, showWarnings = FALSE)
-  unzip(zip, exdir = dossier_sortie)
-  unlink(zip)
-})
+# Extract the zip file
+unzip(zip_destination, exdir = destination_folder)
+
+message("Downloaded and extracted shapefiles to: ", destination_folder)
+
+# Note: After extraction, the shapefile path that will be used in subsequent scripts is:
+# shp_folder_path <- "data-raw/CF_CA_2023_FR-SHP"
+# shapefile_path <- file.path(shp_folder_path, "CF_CA_2023_FR.shp")
--- data-raw/canada_2022_electoral_ridings/02_load_shapefiles_as_rda_to_package.R
+++ data-raw/canada_2022_electoral_ridings/02_load_shapefiles_as_rda_to_package.R
@@ -1,70 +1,43 @@
-## This script loads the shapefiles from each province and merges them together.
-## To obtain access to the data folder containing the downloaded shapefiles, contact Hubert Cadieux
-
+## This script loads the 2023 Canadian federal electoral districts shapefile
+## and saves it as an R data object for the package.
 # Packages ---------------------------------------------------------------
 library(dplyr)
-
-# List files to merge together -------------------------------------------------------------
-files <- list.files(
-  "data-raw/data/canada_2022_electoral_ridings/shapefiles/",
-  pattern = ".shp$",
-  full.names = TRUE,
-  recursive = TRUE
-)
-
-# Merge files together -------------------------------------------------------------
-
-df_shapefiles_together <- lapply(
-  files,
-  FUN = function(x){
-    df <- sf::st_read(x)
-    print(x)
-    return(df)
-  }
-) |> 
-  dplyr::bind_rows() |> 
-  select(
-    id_riding = FED_NUM,
-    name_riding_en = ED_NAMEE,
-    name_riding_fr = ED_NAMEF,
-    id_province = PROV_CODE,
+library(sf)
+shp_folder_path <- "data-raw/data/canada_2022_electoral_ridings/"
+shapefile_path <- file.path(shp_folder_path, "CF_CA_2023_FR.shp")
+spatial_canada_2022_electoral_ridings <- sf::st_read(shapefile_path) |>
+  # Convert to proper projection if needed
+  # sf::st_transform(4326) |> # Uncomment if you need to transform the CRS
+  # Select and rename columns to match the expected structure
+  dplyr::select(
+    id_riding = NUM_CF,      # NUM_CF est la colonne d'ID de circonscription
+    name_riding_fr = CF_NOMFR, # CF_NOMFR est la colonne du nom français
+    name_riding_en = CF_NOMAN, # CF_NOMAN est la colonne du nom anglais
     geometry
-  )
-
-#### Il semble avoir des doublons, mais après vérifications
-#### c'est simplement pour gérer les circonscriptions où le territoire est séparé par de l'eau ou wtv.
-#### Exemple: 24020 et 24023 (Côte-Nord et Gaspésie)
-
-### AJOUTER LES TERRITOIRES
-#### Puisque les territoires n'ont pas été affectés par le redécoupage, le site web où
-#### je suis allé chercher les shapefiles des nouvelles circonscriptions n'a pas inclus les territoires.
-#### Il faut donc aller les chercher du redécoupage précédent, celui de 2013.
-#### On peut aller extraire les shapefiles de 2013 dans le script `data-raw/canada_2013_electoral_ridings/01_extract_shapefiles.R`
-
-df_territories_shapefiles <- sf::st_read("data-raw/data/canada_2013_electoral_ridings/files/lfed000a16a_e.shp") |> 
-  ## filter for territories only
-  filter(
-    `FEDUID` %in% c(
-      60001,
-      61001,
-      62001
-      )
-  ) |> 
+  ) |>
+  # Ajouter le code de province basé sur le préfixe de id_riding
   mutate(
     id_province = case_when(
-      `FEDUID` == 60001 ~ "YT",
-      `FEDUID` == 61001 ~ "NT",
-      `FEDUID` == 62001 ~ "NU"
+      substr(id_riding, 1, 2) == "10" ~ "NL", # Newfoundland and Labrador
+      substr(id_riding, 1, 2) == "11" ~ "PE", # Prince Edward Island
+      substr(id_riding, 1, 2) == "12" ~ "NS", # Nova Scotia
+      substr(id_riding, 1, 2) == "13" ~ "NB", # New Brunswick
+      substr(id_riding, 1, 2) == "24" ~ "QC", # Quebec
+      substr(id_riding, 1, 2) == "35" ~ "ON", # Ontario
+      substr(id_riding, 1, 2) == "46" ~ "MB", # Manitoba
+      substr(id_riding, 1, 2) == "47" ~ "SK", # Saskatchewan
+      substr(id_riding, 1, 2) == "48" ~ "AB", # Alberta
+      substr(id_riding, 1, 2) == "59" ~ "BC", # British Columbia
+      substr(id_riding, 1, 2) == "60" ~ "YT", # Yukon
+      substr(id_riding, 1, 2) == "61" ~ "NT", # Northwest Territories
+      substr(id_riding, 1, 2) == "62" ~ "NU", # Nunavut
+      TRUE ~ "Unknown"
     )
-  ) |> 
-  select(
-    id_riding = `FEDUID`,
-    name_riding_en = `FEDENAME`,
-    name_riding_fr = `FEDFNAME`,
-    id_province,
-    geometry
   )
-
-spatial_canada_2022_electoral_ridings <- rbind(df_shapefiles_together, df_territories_shapefiles)
-
-save(spatial_canada_2022_electoral_ridings, file = "data/spatial_canada_2022_electoral_ridings.rda", compress = "bzip2")
+# Note: The new shapefile might have different column names than what was used before.
+# If the column selections above don't match, you'll need to adjust them based on the
+# actual column names in the shapefile.
+# Save the processed data ------------------------------------------------
+save(spatial_canada_2022_electoral_ridings,
+     file = "data/spatial_canada_2022_electoral_ridings.rda",
+     compress = "bzip2")
